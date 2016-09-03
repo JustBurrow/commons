c3efdc23caea8f8a3e74177144502c26d49e6f8c
Just Burrow <just.burrow@lul.kr>
2016-09-04 00:12:13 +0900

    #8: 일반적인 도메인 오브젝트용 타입 추가.
    Task-Url: https://github.com/JustBurrow/common/issues/8

---
+++ src/main/java/kr/lul/data/IntIdDomainObject.java
@@ -0,0 +1,17 @@
+/**
+ * @see https://github.com/JustBurrow/common
+ */
+package kr.lul.data;
+
+/**
+ * @author Just Burrow
+ * @since 2016. 9. 3.
+ */
+public interface IntIdDomainObject extends Updatable {
+  /**
+   * @return
+   * @author Just Burrow
+   * @since 2016. 9. 3.
+   */
+  public int getId();
+}
