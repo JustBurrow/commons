612eb870271d03a732411b4704edfff945a9b801
Just Burrow <just.burrow@lul.kr>
2018-09-25 00:34:17 +0900

    (ADD) 임의의 시각 데이터를 생성하는 `TimeUtils` 추가.
    테스트용 데이터를 실행중 생성하기 위한 유틸리티.

---
+++ commons-test/src/main/java/kr/lul/commons/test/TimeUtils.java
@@ -0,0 +1,126 @@
+package kr.lul.commons.test;
+
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.concurrent.ThreadLocalRandom.current;
+
+/**
+ * @author justburrow
+ * @since 2018. 9. 24.
+ */
+public abstract class TimeUtils {
+    /**
+     * @return 임의의 인스턴트.
+     */
+    public static Instant instant() {
+        ThreadLocalRandom random = current();
+        return Instant.ofEpochSecond(random.nextLong(-31557014167219200L, 31556889864403200L),
+                                     random.nextLong(1000_000_000L));
+    }
+
+    /**
+     * @return 임의의 시간대 ID.
+     */
+    public static ZoneId zoneId() {
+        List<String> zoneIds = new ArrayList<>(ZoneId.SHORT_IDS.values());
+
+        return ZoneId.of(zoneIds.get(current().nextInt(zoneIds.size())));
+    }
+
+    /**
+     * @return 임의의 {@link ZoneOffset}.
+     */
+    public static ZoneOffset zoneOffset() {
+        return ZoneOffset.ofTotalSeconds(current().nextInt(-64800, 64801));
+    }
+
+    /**
+     * @return 임의의 {@link ZonedDateTime}.
+     */
+    public static ZonedDateTime zonedDateTime() {
+        return instant().atZone(zoneId());
+    }
+
+    /**
+     * @return 임의의 {@link OffsetDateTime}.
+     */
+    public static OffsetDateTime offsetDateTime() {
+        return OffsetDateTime.ofInstant(instant(), zoneId());
+    }
+
+    /**
+     * @return 임의의 {@link LocalDateTime}.
+     */
+    public static LocalDateTime localDateTime() {
+        ThreadLocalRandom random = current();
+
+        LocalDateTime ldt = null;
+        do {
+            try {
+                ldt = LocalDateTime.of(random.nextInt(), random.nextInt(1, 13), random.nextInt(1, 31),
+                                       random.nextInt(24), random.nextInt(60), random.nextInt(60),
+                                       random.nextInt(1000_000));
+            } catch (DateTimeException e) {
+            }
+        } while (null == ldt);
+
+        return ldt;
+    }
+
+    /**
+     * @return 임의의 {@link LocalDate}.
+     */
+    public static LocalDate localDate() {
+        ThreadLocalRandom random = current();
+
+        LocalDate ld = null;
+        do {
+            try {
+                ld = LocalDate.of(random.nextInt(), random.nextInt(1, 13), random.nextInt(1, 31));
+            } catch (DateTimeException e) {
+            }
+        } while (null == ld);
+
+        return ld;
+    }
+
+    /**
+     * @return 임의의 {@link LocalTime}.
+     */
+    public static LocalTime localTime() {
+        ThreadLocalRandom random = current();
+
+        LocalTime lt = null;
+        do {
+            try {
+                lt = LocalTime.of(random.nextInt(0, 23), random.nextInt(60), random.nextInt(60),
+                                  random.nextInt(1_000_000));
+            } catch (DateTimeException e) {
+            }
+        } while (null == lt);
+
+        return lt;
+    }
+
+    /**
+     * @return 임의의 {@link Period}.
+     */
+    public static Period period() {
+        ThreadLocalRandom random = current();
+        return Period.of(random.nextInt(), random.nextInt(), random.nextInt());
+    }
+
+    /**
+     * @return 임의의 {@link Duration}.
+     */
+    public static Duration duration() {
+        return Duration.ofSeconds(current().nextLong(), current().nextLong());
+    }
+
+    private TimeUtils() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ commons-test/src/test/java/kr/lul/commons/test/TimeUtilsTest.java
@@ -0,0 +1,148 @@
+package kr.lul.commons.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.time.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author justburrow
+ * @since 2018. 9. 25.
+ */
+public class TimeUtilsTest {
+    private static final Logger log = getLogger(TimeUtilsTest.class);
+    public static final int LOOP_COUNT = 1000;
+
+    @Test
+    public void testInstant() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            Instant actual = TimeUtils.instant();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testZoneId() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            ZoneId actual = TimeUtils.zoneId();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testZoneOffset() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            ZoneOffset actual = TimeUtils.zoneOffset();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testZonedDateTime() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            ZonedDateTime actual = TimeUtils.zonedDateTime();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testOffsetDateTime() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            OffsetDateTime actual = TimeUtils.offsetDateTime();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testLocalDateTime() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            LocalDate actual = TimeUtils.localDate();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testLocalDate() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            LocalDate actual = TimeUtils.localDate();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testLocalTime() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            LocalTime actual = TimeUtils.localTime();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testPeriod() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            Period actual = TimeUtils.period();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+
+    @Test
+    public void testDuration() throws Exception {
+        for (int i = 0; i < LOOP_COUNT; i++) {
+            // When
+            Duration actual = TimeUtils.duration();
+            log.info("WHEN - actual={}", actual);
+
+            // Then
+            assertThat(actual)
+                    .isNotNull();
+        }
+    }
+}
